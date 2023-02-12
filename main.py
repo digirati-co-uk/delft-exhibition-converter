42c1ecdba44030acd37e6c9aba1fcd922f68f115
tomcrane <tom@nanoplanet.com>
2023-02-12 11:34:32 +0000

    keep labels and summaries on painting annos, point with describing annos

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ ANNOS_WITH_NO_DESC = []
 # See https://github.com/IIIF/iiif-av/issues/27
 # and see https://gist.github.com/stephenwf/a1339aa170a2e80fa120f86027b89f46
 # via https://digirati.slack.com/archives/D0E15T142/p1663263077484059
-# YOUTUBE_CONVERT = "Objectifier"  # Tells the client what <object> tag to render
-YOUTUBE_CONVERT = "Service"  # lets the client decide but client must recognise form
+YOUTUBE_CONVERT = "Objectifier"  # Tells the client what <object> tag to render
+# YOUTUBE_CONVERT = "Service"  # lets the client decide but client must recognise form
 
 
 def convert_folder(old_folder):
@@ -124,20 +124,26 @@ def convert_canvas(canvas):
 
 def convert_tour_steps_to_descriptive_annos(canvas):
     """
-    If an anno under canvas.items has a label and summary, it needs to become a describing anno under
-    canvas.annotations[0].items.
-    We still might preserve the label of the annotation though, for ease of use
+    painting annotations (check that the body is an image) under canvas.items retain their labels and summaries.
+
+    canvases retain their labels, summaries and required statement.
 
     If the anno body is a TextualBody it gets removed from the painting annos altogether, it's ONLY a describing anno.
     Tours will be run from the describing annos, moving the user around the canvas.
 
-    PRESERVE_ANNO_LABELS keeps the label property on the
+    If a canvas.items only has one anno and it's a painting anno, leave as-is (?) - it's not a tour!
+    You don't need to make a describing anno for it.
 
+    motivation["describing"]
     """
     anno_page = required_single_item(canvas)
     for_removal = []
     expected_body_types = ["Image", "Video", "TextualBody"]
     for anno in anno_page["items"]:
+
+        if anno["body"]["id"] == "https://dlc.services/iiif-img/7/6/acd52dad-b8b1-4afa-9ddf-77ee85397003/376,407,1042,758/full/0/default.jpg":
+            print("here")
+
         if anno["body"]["type"] not in expected_body_types:
             raise ValueError(f"Unexpected anno body type: {anno['body']['type']}")
 
@@ -145,9 +151,20 @@ def convert_tour_steps_to_descriptive_annos(canvas):
 
         label = anno.get("label", None)
         summary = anno.get("summary", None)
-        if label is not None or summary is not None:
+
+        if label is None and summary is None:
+            # maps are an example of this
+            # https://delft-static-site-generator.netlify.com/iiif/1f05178d-9382-53b9-cd33-86ffd19f0476/canvas/f5868dbe-e170-33d5-1623-9aa2dfbb6635
+            # they stay as plain painting annos but for now we'll still make them tour steps
+            # raise ValueError(f"anno without label or summary: {anno}")
+            pass
+
+        if (label is not None or summary is not None) and len(anno_page["items"]) > 1:
+            # For our initial migration, it's both a painting anno and a tour step
+            # But later we can have tour steps that are not painting annos
             annotations = canvas.get("annotations", None)
             if annotations is None:
+                # create an annotations property for the canvas
                 annotations = [{
                     "type": "AnnotationPage",
                     "id": f"{canvas['id']}/annotations",
@@ -157,44 +174,39 @@ def convert_tour_steps_to_descriptive_annos(canvas):
             describing_anno = {
                 "id": f"{canvas['id']}/annotations/{ITEMS_ANNOS_COUNT}",
                 "type": "Annotation",
-                "motivation": "describing",
-                "target": anno["target"]
+                "motivation": "describing",  # says "this is a tour", but we could have a "tour" motivation
+                "target": {  # target the painting annotation
+                    "id": anno["id"],
+                    "type": "Annotation"
+                }
+                # There is no "body", just use the info on the painting anno - unless it's a TextualBody
             }
 
             canvas["annotations"][0]["items"].append(describing_anno)
 
-            id_root = f"{anno['id']}/desc"
-            describing_anno["body"] = []
             nl_body = get_html_from_label_and_summary(anno, "nl", True)
-            if nl_body is not None:
-                describing_anno["body"].append(make_textual_body(nl_body, "text/html", "nl", f"{id_root}/nl"))
             en_body = get_html_from_label_and_summary(anno, "en", True)
-            if en_body is not None:
-                describing_anno["body"].append(make_textual_body(en_body, "text/html", "en", f"{id_root}/en"))
-
             if nl_body is None and en_body is None:
-                # If there's no summary and we're preserving labels, we might have a tour step with no textual content
+                # log for information
                 ANNOS_WITH_NO_DESC.append(anno["id"])
-
-            if label is not None and not PRESERVE_ANNO_LABELS:
-                del anno["label"]
-            if summary is not None:
-                del anno["summary"]
-
-            if anno["body"]["type"] == "TextualBody":
-                for_removal.append(anno)
-            else:
-                describing_anno["scope"] = anno["id"]  # reference the painting anno this is for
+            elif anno["body"]["type"] == "TextualBody":
+                for_removal.append(anno)  # later we'll take this out of canvas.items
+                id_root = f"{anno['id']}/desc"
+                describing_anno["body"] = []
+                describing_anno["target"] = anno["target"]
+                if nl_body is not None:
+                    describing_anno["body"].append(make_textual_body(nl_body, "text/html", "nl", f"{id_root}/nl"))
+                if en_body is not None:
+                    describing_anno["body"].append(make_textual_body(en_body, "text/html", "en", f"{id_root}/en"))
 
     if len(for_removal) > 0:
-        print(f"{len(for_removal)} describing annos to remove")
+        print(f"{len(for_removal)} TextualBody annos to remove from the painting annos")
         for moved in for_removal:
             anno_page["items"].remove(moved)
             REMOVED_ANNOS.append(moved["id"])
 
 
 def update_anno_counters(anno):
-
     global ITEMS_ANNOS_COUNT
     global ITEMS_ANNOS_WITH_LABELS_COUNT
     global ITEMS_ANNOS_WITH_SUMMARIES_COUNT
@@ -321,7 +333,7 @@ def move_thumbnails_from_painting_annos_to_their_bodies(canvas):
         if anno["motivation"] != "painting":
             raise ValueError(f"Unexpected motivation '{anno['motivation']}' in canvas.items")
         if anno["body"]["type"] != "Image" and anno["body"]["type"] != "Video":
-            raise ValueError(f"Unexpected body type '{ anno['body']['type']}' in canvas.items")
+            raise ValueError(f"Unexpected body type '{anno['body']['type']}' in canvas.items")
         thumbnail = anno.get("thumbnail", None)
         if thumbnail is None or PAINTING_ANNO_THUMBS == "Annotation":
             return
@@ -335,9 +347,10 @@ def move_thumbnails_from_painting_annos_to_their_bodies(canvas):
 def remodel_cropped_painting_annos(canvas):
     anno_page = required_single_item(canvas)
     for anno in anno_page["items"]:
+
         if anno.get("body.id", None) is None:
             # The body.id property is how the old editor signified a crop
-            return
+            continue
 
         region = anno["body"]["id"].split("/")[-4]
         anno["body"]["id"] = anno["body"]["id"].replace(f"/{region}/", "/full/")
@@ -356,7 +369,6 @@ def remodel_cropped_painting_annos(canvas):
 
 
 def remodel_av_and_3d_painting_annos(canvas):
-
     anno_page = required_single_item(canvas)
     youtube = "https://www.youtube.com"
     youtube_short = "https://youtu.be"  # transform this!
@@ -407,7 +419,7 @@ def remodel_av_and_3d_painting_annos(canvas):
                 "type": "Image",
                 "format": "image/jpg"
             }
-            img_service =  anno["thumbnail"][0].get("service", None)
+            img_service = anno["thumbnail"][0].get("service", None)
             if img_service is not None and img_service.get("type", None) is not None:
                 # some weird IIIF for the sketchfab
                 anno["body"]["service"] = [img_service]
